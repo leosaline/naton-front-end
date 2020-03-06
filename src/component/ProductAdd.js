267fcd7d2a1af6572e7c94852fb4962272c811ab
leosaline <leosaline@gmail.com>
2020-03-06 19:23:23 -0300

    add jquery

--- src/component/ProductAdd.js
+++ src/component/ProductAdd.js
@@ -13,16 +13,13 @@ class ProductAdd extends React.Component {
     render() {
         return (
             <div>
-
-                
-
+                <br /><br />
                 <div className="input-group mb-3">
                     <div className="input-group-prepend">
                         <span className="input-group-text" id="basic-addon1">name</span>
                     </div>
                     <input type="text" className="form-control" placeholder="type product name" aria-label="name" aria-describedby="basic-addon1" />
                 </div>
-
                 <div className="input-group mb-3">
                     <div className="input-group-prepend">
                         <label className="input-group-text" htmlFor="inputGroupSelect01">Options</label>
@@ -34,13 +31,28 @@ class ProductAdd extends React.Component {
                         <option value="3">Three</option>
                     </select>
                 </div>
-
                 <div className="input-group mb-3">
                     <div className="input-group-prepend">
                         <span className="input-group-text" id="basic-addon1">Package Type</span>
                     </div>
                     <input type="text" className="form-control" placeholder="select package type" aria-label="packageType" aria-describedby="basic-addon1" />
                 </div>
+
+
+                <p>
+                    <button className="btn btn-primary" type="button" data-toggle="collapse" data-target="#collapseAddProduct" aria-expanded="false" aria-controls="collapseAddProduct">
+                        Add Product
+                    </button>
+                </p>
+                <div className="collapse" id="collapseAddProduct">
+                    <div className="card card-body">
+                        <h1>teste teste</h1>
+                    </div>
+                </div>
+
+
+
+
             </div>
         );
     }
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import 'bootstrap/dist/css/bootstrap.css';
+
 // Put any other imports below so that CSS from your
 // components takes precedence over default styles.
 
