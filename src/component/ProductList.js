1390b1576bef9fc56fbc7a88b2e647f8873c5d11
leosaline <leosaline@gmail.com>
2020-03-05 22:23:39 -0300

    simple menu

--- src/component/ProductList.js
+++ src/component/ProductList.js
@@ -1,8 +1,8 @@
 import React from 'react';
 
-class ProductList extends React.Component{
+class ProductList extends React.Component {
 
-    constructor(props){
+    constructor(props) {
         super(props);
         this.state = {
             error: null,
@@ -13,40 +13,75 @@ class ProductList extends React.Component{
 
     componentDidMount() {
         fetch("http://localhost:8080/products")
-          .then(res => res.json())
-          .then(
-            (result) => {
-              this.setState({
-                isLoaded: true,
-                items: result
-              });
-            },
-            (error) => {
-              this.setState({
-                isLoaded: true,
-                error
-              });
-            }
-          )
-      }
+            .then(res => res.json())
+            .then(
+                (result) => {
+                    this.setState({
+                        isLoaded: true,
+                        items: result
+                    });
+                },
+                (error) => {
+                    this.setState({
+                        isLoaded: true,
+                        error
+                    });
+                }
+            )
+    }
 
-      render() {
+    render() {
         const { error, isLoaded, items } = this.state;
         if (error) {
-          return <div>Error: {error.message}</div>;
+            return <div>Error: {error.message}</div>;
         } else if (!isLoaded) {
-          return <div>Loading...</div>;
+            return <div>Loading...</div>;
         } else {
-          return (
-            <ul>
-              {items.map(item => (
-                <li key={item.name}>
-                  {item.name} {item.PackageType} {item.company}
-                </li>
-              ))}
-            </ul>
-          );
+            return (
+                <div>
+                    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                        <a class="navbar-brand" href="http://localhost:3000">Naton</a>
+                        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+                            <span class="navbar-toggler-icon"></span>
+                        </button>
+                        <div class="collapse navbar-collapse" id="navbarText">
+                            <ul class="navbar-nav mr-auto">
+                                <li class="nav-item active">
+                                    <a class="nav-link" href="http://localhost:3000">Home <span class="sr-only">(current)</span></a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="http://localhost:3000">Product</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="http://localhost:3000">Purchase</a>
+                                </li>
+                            </ul>
+                            <span class="navbar-text">
+                                Naton Beauty Systen.
+                            </span>
+                        </div>
+                    </nav>
+                    <table class="table table-striped">
+                        <thead class="thead-dark">
+                            <tr>
+                                <th scope="col">#</th>
+                                <th scope="col">name</th>
+                                <th scope="col">package type</th>
+                                <th scope="col">company</th>
+                            </tr>
+                        </thead>
+                        {items.map(item => (
+                            <tr>
+                                <td>{item.id}</td>
+                                <td>{item.name}</td>
+                                <td>{item.packageType}</td>
+                                <td>{item.company}</td>
+                            </tr>
+                        ))}
+                    </table>
+                </div>
+            );
         }
-      }
+    }
 }
 export default ProductList;
